6bea80a0369f5a5323e8878fddd28b82e4dd99fc
elifimkalp <toylansamet2@gmail.com>
2023-08-21 05:06:12 +0300

    text files

--- script/main.js
+++ script/main.js
@@ -4,6 +4,18 @@ const fetchData = () => {
     .then((data) => {
       dataArr = Object.keys(data);
       dataArr.map((customData) => {
+        if (data[customData] !== "") {
+          if (customData === "imagePath") {
+            document
+              .querySelector(`[data-node-name*="${customData}"]`)
+              .setAttribute("src", data[customData]);
+          } else {
+            document.querySelector(
+              `[data-node-name*="${customData}"]`
+            ).innerText = data[customData];
+          }
+        }
+
         // Check if the iteration is over
         // Run amimation if so
         if (dataArr.length === dataArr.indexOf(customData) + 1) {
